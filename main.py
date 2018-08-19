cebcd20294c33708c608bb553047ffa8724e704a
Dremalka <dremalka@ua.fm>
2018-08-19 14:47:39 +0700

    _

--- main.py
+++ main.py
@@ -18,9 +18,8 @@ class TvList:
 
 tvl = TvList()
 tvp = TvProgram()
-for ch in tvp.getchannels():
-    for child in ch.find("a", {"href": True}):
-        print(child)
-#html = urlopen("http://www.pythonscraping.com/pages/page3.html")
-#bsObj = BeautifulSoup(html, 'html.parser')
-#print(bsObj.find("img", {"src": "../img/gifts/img1.jpg"}).parent.previous_sibling.get_text())
+
+html = urlopen("http://www.cn.ru/tv/")
+bsobj = BeautifulSoup(html, 'html.parser')
+allch = bsobj.find_all("div", {"class": "mtv-lastrec tdr"})
+
